9af0ebe0f3eaf1b1aa068f01f217d270ea7c844d
974 <icrawfo89@gmail.com>
2016-05-02 18:10:08 -0400

    Add user microposts

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -4,9 +4,12 @@ class PasswordResetsController < ApplicationController
   before_action :check_expiration, only: [:edit, :update]
 
   def new
+  end
+
+  def create
     @user = User.find_by(email: params[:password_reset][:email].downcase)
     if @user
-      @user.create_reset_diget
+      @user.create_reset_digest
       @user.send_password_reset_email
       flash[:info] = "Email sent with password reset instructions"
       redirect_to root_url
@@ -15,12 +18,10 @@ class PasswordResetsController < ApplicationController
       render 'new'
     end
   end
-  
-  def 
 
   def edit
   end
-  
+
   def update
     if params[:user][:password].empty?
       @user.errors.add(:password, "can't be empty")
@@ -33,28 +34,32 @@ class PasswordResetsController < ApplicationController
       render 'edit'
     end
   end
-  
+
   private
-  
+
     def user_params
       params.require(:user).permit(:password, :password_confirmation)
     end
-  
+
+    # Before filters
+
     def get_user
       @user = User.find_by(email: params[:email])
     end
-    
+
+    # Confirms a valid user.
     def valid_user
-      unless (@user && @user.activated? && @user.authenticated?(:reset, params[:id])
+      unless (@user && @user.activated? &&
+              @user.authenticated?(:reset, params[:id]))
         redirect_to root_url
       end
     end
-    
+
+    # Checks expiration of reset token.
     def check_expiration
       if @user.password_reset_expired?
         flash[:danger] = "Password reset has expired."
         redirect_to new_password_reset_url
       end
     end
-    
 end
